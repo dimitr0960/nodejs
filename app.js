25f6c475f93dbc81f7d5ca40ee4cd8c52158d1f9
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2023-01-22 16:02:33 +0300

    lesson_05

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-const fs = require("fs");
+const Logger = require("./log");
 
-fs.readFile("./test.txt", "utf8", (error, data) => {
-	// console.log(data.toString());
-	console.log(data); // with utf8
+const logger = new Logger();
+
+logger.on("some_event", (args) => {
+	const { id, text } = args;
+	console.log(id, text);
 });
 
-console.log("First text");
-
-fs.readFile("./test.txt", "utf8", (error, data) => {
-	fs.mkdir("./files", () =>{
-		fs.writeFile("./files/test2.txt", `${data} New text!`, () => {
-
-		});
-	});
-});
-
-// вложенность, либо async await
-// либо mkdirSync / writeFileSync
-
-setTimeout(() => {
-	if (fs.existsSync("./files/test2.txt")) {
-		fs.unlink("./files/test2.txt", () => {});
-	}
-}, 4000);
-
-setTimeout(() => {
-	if (fs.existsSync("./files")) {
-		fs.rmdir("./files", () => {});
-	}
-}, 6000);
-
-
-
+logger.log("User logged");
