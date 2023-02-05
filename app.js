aa690ac65cfdab801fec78da111f3b26c083534c
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2023-02-05 15:18:03 +0300

    lesson_06

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const Logger = require("./log");
+const fs = require("fs")
+const zlib = require("zlib")
 
-const logger = new Logger();
+const readStream = fs.createReadStream("./docs/text.txt")
+const writeSteam = fs.createWriteStream("./docs/new-text.txt")
+const compressStream = zlib.createGzip()
 
-logger.on("some_event", (args) => {
-	const { id, text } = args;
-	console.log(id, text);
-});
+// readStream.on("data", (chunk) => {
+// 	writeSteam.write("\n---CHUNK START---\n")
+// 	writeSteam.write(chunk)
+// 	writeSteam.write("\n---CHUNK END---\n")
+// })
 
-logger.log("User logged");
+// readStream.pipe(writeSteam)
+
+const handleError = () => {
+	console.log("Error")
+	readStream.destroy()
+	writeSteam.end("Finished with error...")
+}
+
+readStream
+	.on("error", handleError)
+	.pipe(compressStream)
+	.pipe(writeSteam)
+	.on("error", handleError)
+
