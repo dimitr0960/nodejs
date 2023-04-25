e8de664333d58ac87048c436d07ab51f5db978c4
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2023-04-25 17:02:03 +0300

    lesson_15

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const path = require("path")
 const morgan = require("morgan")
+const mongoose = require("mongoose")
+const Post = require("./models/post")
 
 const app = express()
 
 app.set("view engine", "ejs")
 
 const PORT = 3000
+const db = 'mongodb+srv://dimitr0960:Pass321@cluster0.v3ufx8h.mongodb.net/node-blog?retryWrites=true&w=majority'
+
+mongoose
+	.connect(db)
+	.then((res) => console.log("Connected to DB"))
+	.catch((error) => console.log(error))
 
 const createPath = (page) => {
 	return path.resolve(__dirname, "ejs-views", `${page}.ejs`)
@@ -63,14 +71,15 @@ app.get("/posts", (req, res) => {
 
 app.post("/add-post", (req, res) => {
 	const { title, author, text } = req.body
-	const post = {
-		id: new Date(),
-		date: (new Date()).toLocaleDateString(),
-		title,
-		author,
-		text,
-	}
-	res.render(createPath("post"), { post, title })
+	const post = new Post({ title, author, text })
+	post
+		.save()
+		.then((result) => res.send(result))
+		.catch((error) => {
+			console.log(error)
+			res.render(createPath("error"), { title: "ERROR" })
+		})
+
 })
 
 app.get("/add-post", (req, res) => {
@@ -84,3 +93,7 @@ app.use((req, res) => {
 		.status(404)
 		.render(createPath("error"), { title })
 })
+
+// dimitr0960
+// PXCaZ4W2yVAuPzEr
+// Pass321
