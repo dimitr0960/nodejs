fde4abf7a274fb92a5e65bba72e84a5510483b46
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2023-04-25 09:49:11 +0300

    lesson_11

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const PORT = 3000
+
+const createPath = (page) => {
+	return path.resolve(__dirname, "views", `${page}.html`)
+}
+
+app.listen(PORT, (error) => {
+	error ? console.log(error) : console.log(`listening post ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+	res.sendFile(createPath("index"))
+})
+
+app.get("/contacts", (req, res) => {
+	res.sendFile(createPath("contacts"))
+})
+
+app.get("/about-us", (req, res) => {
+	res.redirect("/contacts")
+})
+
+app.use((req, res) => {
+	res
+		.status(404)
+		.sendFile(createPath("error"))
+})
