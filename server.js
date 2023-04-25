25a5875367989269cfb4517e88d46d431e283c5a
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2023-04-25 10:38:46 +0300

    lesson_13

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require("path")
+const morgan = require("morgan")
 
 const app = express()
 
@@ -14,6 +15,10 @@ const createPath = (page) => {
 app.listen(PORT, (error) => {
 	error ? console.log(error) : console.log(`listening post ${PORT}`)
 })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+app.use(express.static('styles'))
 
 app.get("/", (req, res) => {
 	const title = "Home"
